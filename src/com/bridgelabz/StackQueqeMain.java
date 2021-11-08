e88ea21e88be4850a2b36bb027d4f2a0cf9ac9ff
NADEEMSHAIKH117 <nadeemshaikh1171998@gmail.com>
2021-11-08 14:46:01 +0530

    added

--- src/com/bridgelabz/StackQueqeMain.java
+++ src/com/bridgelabz/StackQueqeMain.java
@@ -8,6 +8,7 @@ public class StackQueqeMain {
         System.out.println("Press '1' to add element at the start");
         System.out.println("Press '2' to peek the element");
         System.out.println("Press '3' to pop the element");
+        System.out.println("press '4' to add element in the Queqe");
         switch (sc.nextInt()) {
             case 1:
                 Operation.addElement();
@@ -19,6 +20,9 @@ public class StackQueqeMain {
             case 3:
                 Operation.pop();
                 break;
+            case 4:
+                Operation.queueElement();
+                break;    
             default:
             {
                 System.out.println("Press Valid Key");
